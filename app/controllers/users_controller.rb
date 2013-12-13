4ac52cd82fe7797b869c057344462a8ca9af4686
Eli Fatsi <eli.fatsi@viget.com>
2013-12-12 23:09:15 -0700

    Added directions to welcome message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, :notice => "Welcome to the site!"
+      redirect_to root_url, :notice => "Welcome to the site! Click on the days you plan to be at the condo."
     else
       render :new
     end
